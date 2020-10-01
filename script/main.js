e0bc2ac02e89707e5cde76d51a9aa9ce1772a44a
Sarvar <sarvar95yil@gmail.com>
2020-10-01 17:14:41 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,53 @@
+// DAY 1
+
+const btnBurger = document.querySelector('.btn-burger');
+const catalog = document.querySelector('.catalog');
+const btnClose = document.querySelector('.btn-close');
+const catalogList = document.querySelector('.catalog-list');
+const subCatalog = document.querySelector('.subcatalog');
+const subCatalogHeader = document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
+
+function openMenu() {
+    catalog.classList.add('open');
+    overlay.classList.add('active');
+}
+
+function closeMenu() {
+    catalog.classList.remove('open');
+    overlay.classList.remove('active');
+    closeSubMenu();
+}
+
+const openSubMenu = event => {
+    event.preventDefault();
+    const target = event.target;
+    const itemList = target.closest('.catalog-list__item');
+    if (itemList) {
+        subCatalogHeader.innerHTML = target.innerHTML;
+        subCatalog.classList.add('subopen');
+    }
+
+}
+
+const closeSubMenu = () => {
+    subCatalog.classList.remove('subopen');
+}
+
+btnBurger.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+catalogList.addEventListener('click', openSubMenu);
+btnReturn.addEventListener('click', closeSubMenu);
+
+
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+        closeMenu();
+    }
+})
